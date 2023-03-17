7a8073224d8f2be65c234072e274b7149848ec21
Peter McConnell <me@petermcconnell.com>
2023-03-17 12:12:39 +0000

    greedy match screen sessions

--- minecraft/templates/boot_server.sh
+++ minecraft/templates/boot_server.sh
@@ -14,7 +14,10 @@ RAM=$2
 
 SCREEN_NAME="zcraft_$FOLDER"
 
-screen -XS $(screen -ls | grep -o -e "[0-9]\+\.$SCREEN_NAME") quit || true
+for scr in $(screen -ls | grep -o -e "[0-9]\+\.[^(]\+" | tr -d ' '); do
+  screen -XS $scr quit || true
+done
+
 screen -wipe || true
 screen -dmS $SCREEN_NAME sh
 screen -S $SCREEN_NAME -X stuff "sh ./boot_server_screen.sh $FOLDER $RAM && exit"
